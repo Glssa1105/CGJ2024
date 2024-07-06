fa79e6bf9287d17dd06e607cb7d95e69bd080ca7
BbxerGarlic <83629459+BbxerGarlic@users.noreply.github.com>
2024-07-06 14:35:57 +0800

    gobro

---
+++ Assets/Scrips/Components/ComponentManager.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class ComponentManager : MonoBehaviour
+{
+    private static ComponentManager instance;
+    public static ComponentManager Instance => instance;
+
+    public List<Item> itemDetails=new List<Item>();
+    
+    public List<ComponentBase> components = new List<ComponentBase>();
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+
+    public SpriteRenderer CreateComponent(Grid grid,int id)
+    {
+        var item=itemDetails.Select((i) => i.id == id);
+        
+        
+        
+        return null;
+    }
+    
+}
